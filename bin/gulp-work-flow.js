1dd85fc5b0c49fcb512cc343877cd38b39f990d4
ling_yu <ling_yu@dnt.com.cn>
2020-06-07 22:52:50 +0800

    feat: update package

--- bin/gulp-work-flow.js
+++ bin/gulp-work-flow.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const pkg = require('../package')
-const gulpWorkFlow = require('..')
+// Node CLI 应用入口文件必须要有上面展示的文件头
+    // 如果系统是 Linux 或者 macOS 系统下还需要修改此文件的读写权限为 755，具体是通过  chomd 755 cli.js 实现修改
 
-program
-  .version(pkg.version)
-  .usage('<input>')
-  .option('-H, --host', 'Email host')
-  .on('--help', console.log)
-  .parse(process.argv)
-  .args.length || program.help()
+process.argv.push('--cwd');
+process.argv.push(process.cwd());
+process.argv.push('--gulpfile');
+// require.resolve('../lib/index')，由于 package.json 中设置了 主入口文件为 lib/index.js，路径可以简写为 ..
+process.argv.push(require.resolve('..'));
+require('gulp/bin/gulp');
 
-const { args, host } = program
-const [ input ] = args
-
-// TODO: Implement module cli
-console.log(gulpWorkFlow(input, { host }))
+// console.log(process.argv); 
+// [
+//   '/usr/local/bin/node',
+//   '/usr/local/bin/gulp-work-flow',
+//   '--cwd',
+//   '/Users/yuling/program/myFiles/ECMAScript/frontend/P2/M1-AutoBuild/gulp/gulp-work-flow',
+//   '--gulpfile',
+//   '/Users/yuling/program/myFiles/ECMAScript/frontend/P2/M1-AutoBuild/gulp/gulp-work-flow/lib/index.js'
+// ]
